6be13afd6779bdc94c3ad8de7d26c74cb698cf4d
MrNomNom98 <tobi.bergmer@gmail.com>
2020-04-12 08:48:05 +0200

    solved some bugs

--- fks/backend/dist/main.js
+++ fks/backend/dist/main.js
@@ -4,7 +4,7 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 const port = process.env.port || 3000 || 80;
-const baseurl = 'http://localhost:';
+const baseurl = 'http://35.234.114.16:';
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
