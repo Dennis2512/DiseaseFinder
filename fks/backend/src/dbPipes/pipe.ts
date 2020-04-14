0b27d0aeb6cc5d8beae8ff032d03b0496ca58190
MrNomNom98 <tobi.bergmer@gmail.com>
2020-04-14 09:16:38 +0200

    tried other function for CRUD operation

--- fks/backend/src/dbPipes/pipe.ts
+++ fks/backend/src/dbPipes/pipe.ts
@@ -1,8 +1,11 @@
 //Hier wird der SymptomInput an die Datenbank gesendet und die Daten an den Client zurück
 import { getConnection } from "typeorm";
+import { createConnection } from "typeorm";
 import { TestEntity } from "./test.entity";
 import { async } from "rxjs/internal/scheduler/async";
+import { Test } from "@nestjs/testing";
 //TEST
+/*
 (async () => {
     try {
         await getConnection()
@@ -26,3 +29,13 @@ import { async } from "rxjs/internal/scheduler/async";
 
     }
 })()
+*/
+createConnection()
+    .then(async conn =>{
+        const sympt1 = await TestEntity
+            .create({disease_name: "Test_disease_Name1",
+                     disease_name_professional: "Test_disease_name_professional1",
+                     description: "Test_discription2",
+                     symptoms: "Test_Symptom2"})
+                     .save();
+    })
