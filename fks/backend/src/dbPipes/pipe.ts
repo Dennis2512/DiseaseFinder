eb80608eb9dcff2bc071886f5d1cb7af22ec56d3
MrNomNom98 <tobi.bergmer@gmail.com>
2020-04-14 07:41:22 +0200

    insert some test data into test table

--- fks/backend/src/dbPipes/pipe.ts
+++ fks/backend/src/dbPipes/pipe.ts
@@ -1,1 +1,24 @@
 //Hier wird der SymptomInput an die Datenbank gesendet und die Daten an den Client zurück
+import {getConnection} from "typeorm";
+import { TestEntity } from "./test.entity";
+import { async } from "rxjs/internal/scheduler/async";
+//TEST
+(async()=>{
+await getConnection()
+    .createQueryBuilder()
+    .insert()
+    .into(TestEntity)
+    .values([
+        { disease_name: "Test_disease_Name1", 
+          disease_name_professional: "Test_disease_name_professional1", 
+          description: "Test_discription1",
+          symptoms: [{"Test_Symptom": "Symptom1"}]
+        }, 
+        { disease_name: "Test_disease_Name2", 
+        disease_name_professional: "Test_disease_name_professional2", 
+        description: "Test_discription2",
+        symptoms: [{"Test_Symptom": "Symptom2"}]
+      }, 
+     ])
+    .execute();
+})()
