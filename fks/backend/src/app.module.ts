4fafd13cc91aa6883f967f3836b6441657ca0fb4
MrNomNom98 <tobi.bergmer@gmail.com>
2020-04-14 05:49:05 +0200

    entity corrected

--- fks/backend/src/app.module.ts
+++ fks/backend/src/app.module.ts
@@ -5,14 +5,21 @@ import { MessagesController } from './messages/messages.controller';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
+import { TestEntity } from './dbPipes/test.entity'
+import  {PraxisEntity} from './dbPipes/praxis.entity'
+import {DiseaseEntity} from './dbPipes/disease.entity'
+import { DbModule } from './dbPipes/db.module';
 
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    //TypeOrmModule.forRoot({autoLoadEntities: true}),
+    //DbModule
+    TypeOrmModule.forRoot({autoLoadEntities: true}),
   ],
   controllers: [AppController, MessagesController],
   providers: [AppService],
+
 })
 export class AppModule {
   constructor(private connection: Connection) {}
