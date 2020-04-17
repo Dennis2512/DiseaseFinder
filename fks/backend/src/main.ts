7dd2564f20fbbcbf674f25be1cad95819c6a0524
MrNomNom98 <tobi.bergmer@gmail.com>
2020-04-17 05:41:53 +0200

    added log for db

--- fks/backend/src/main.ts
+++ fks/backend/src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(port);
-  //Logger.log(`Server running on ${baseurl}${port}`, 'Bootstrap');
+  Logger.log(`Server running on ${baseurl}${port}`, 'Bootstrap');
 }
 bootstrap();
