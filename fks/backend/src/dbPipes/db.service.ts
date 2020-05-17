ea411dd3e31de5b2d25b4754335aa674bed07100
MrNomNom98 <tobi.bergmer@gmail.com>
2020-05-17 23:38:15 +0200

    minor fix

--- fks/backend/src/dbPipes/db.service.ts
+++ fks/backend/src/dbPipes/db.service.ts
@@ -26,7 +26,7 @@ export class DbService {
         private diseaseDeRepository: Repository<DiseaseDe>,
 
         @InjectRepository(Praxis)
-        private praxisRepository: Repository<Praxis>,*//*
+        private praxisRepository: Repository<Praxis>,*/
     ) { }
 
     //CRUD Functions for TestEntity
